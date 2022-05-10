2621c2caedf754e71aec8887f50c43345db86e6d
Diogo Aguiar <44610050+diogomaguiar86@users.noreply.github.com>
2022-05-10 15:40:58 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,335 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+
+<head>
+
+		<link rel="icon" href="../img/icon_concre.png" type="image/png">
+
+		
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Source code generated using layoutit.com">
+		<meta name="author" content="LayoutIt!">
+		
+		<title>INTRANET CONCRESERV</title>
+
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+
+		<div style="text-align: left">
+		<div  class="container-fluid">
+		<nav class="row" style="background-color: white">
+		<div class="col-md-12" style="text-align: center">
+							
+		<a  class="navbar-brand" href="#"><img src="../img/LOGOTIPO.jpg" alt="..."></a>
+						
+
+		<ul class="nav mr-auto" style="justify-content: center;">
+			<div><li class="nav-item"><a style="color: black" class="nav-link active" href="index.php"> HOME </a></li></div>
+			<div><li class="nav-item"><a style="color: black" class="nav-link active" href="ramais.php"> RAMAIS </a></li></div>
+			<div><li class="nav-item"><a style="color: black" class="nav-link active" href="cozinha.php"> COZINHA </a></li></div>
+			<div><li class="nav-item"><a style="color: black" class="nav-link active" href="classificados.php"> ANÚNCIOS </a></li></div>
+			<div><li class="nav-item"><a style="color: black" class="nav-link active" href="faq.php"> FAQs </a></li></div>
+		
+			<container>
+				<div class="container-fluid" style="text-align: center;">			
+					<div class="btn btn-outline-dark" aria-label="Grupo de botões com dropdown aninhado">
+						<div role="group">
+										
+							<button id="btnGroupDrop1" 
+								type="button" 
+								class="btn btn-danger dropdown-toggle" 
+								data-toggle="dropdown" 
+								aria-haspopup="true" 
+								aria-expanded="false">   
+								LINKS ÚTEIS   
+							</button>
+										
+
+							<div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+											
+								<li class="nav-item"><a class="nav-link active" target="_blank" href="https://portalcliente.concreserv.com.br/login">PORTAL CONCRESERV</a></li>
+								<li class="nav-item"><a class="nav-link active" target="_blank" href="https://concreserv.com.br/">SITE CONCRESERV</a></li>
+								<li class="nav-item"><a class="nav-link active" target="_blank" href="https://homeoffice.concreserv.com.br/">HOME OFFICE</a></li>
+								<li class="nav-item"><a class="nav-link active" target="_blank" href="https://www.telemachine.com.br/tmwebcli/pvt/Rastreamento.do/">TELEMACHINE</a></li>
+								<li class="nav-item"><a class="nav-link active" target="_blank" href="https://mx05.concreserv.com.br/brightmail/viewLogin.do">ANTI SPAM</a></li>
+								<li class="nav-item"><a class="nav-link active" target="_blank" href="http://10.10.0.11/betonMIXProducao/webforms/wFrmLogin.aspx?ReturnUrl=/BetonMixProducao/webforms/wFrmPrincipal.aspx">BETON MIX</a></li>
+								<li class="nav-item"><a class="nav-link active" target="_blank" href="https://concreserv.2easyapp.com.br//">2 EAZY </a></li>
+								<li class="nav-item"><a class="nav-link active" target="_blank" href="https://concreserv.com.br/usinas-de-concreto/">ÚSINAS</a></li>
+								<li class="nav-item"><a class="nav-link active" target="_blank" href="https://app.goto.com/landing">GO TO</a></li>
+								<li class="nav-item"><a class="nav-link active" target="_blank" href="https://concreserv.virtual360.io/users/sign_in"> V360º </a></li>
+								<li class="nav-item"><a class="nav-link active" target="_blank" href="https://app.ideacrm.com.br/Home.aspx/">IDEA</a></li>
+								<li class="nav-item"><a class="nav-link active" target="_blank" href="https://secure.d4sign.com.br/">D4S</a></li>
+
+							</div>
+						</div>
+					</div>
+				</div>
+								</ul>
+								</div>
+								</div>
+								</div>
+								</nav>
+								</div>
+								</div>
+								</div>
+								</container>
+</head>
+
+<body>
+				<div class="container-fluid">
+					<div class="row">
+					<div class="col-md-12">
+					<div class="row">
+								
+							
+					<div class="col-md-4">
+					<div class="card">
+					<div class="card-body">
+					<div class="card-text" style="text-align: center">
+					<img src="../img/CHEGOU.gif" width=100%>
+					<a class="btn btn-light text-align: right"
+						style="color: red" 
+						target="_blank" 
+						href="https://senha.concreserv.com.br/authorization.do"><img src="../img/icone-reset 2.png"/> </a>
+					</div>
+					</div>
+					</div>
+					</div>
+	
+	
+					<div class="col-md-4">
+					<div class="card">
+					<div class="card-body">
+					<div class="card-text" style="text-align: center" width=110%>
+					<img src="../img/carosel03.gif" width=100%>
+					<a class="btn btn-light text-align: right"
+						style="color: red" target="_blank" 
+						href="https://g1.globo.com/sp/sao-paulo/video/concreserv-diz-que-mais-de-mil-caminhoes-de-concreto-serao-usados-no-reparo-da-cratera-em-sp-10262671.ghtml"><img src="../img/g1-icon.png"/>
+					</a>
+					</div>
+					</div>
+					</div>
+					</div>
+				
+	
+				
+					<div class="col-md-4">
+					<div class="card">
+					<div class="card-body">
+					<div class="card-text" style="text-align: center">
+					<img src="../img/11.jpg" width=100%>
+					<a class="btn btn-light text-align: right"
+						style="color: red" target="_blank" 
+						href="https://concreserv.com.br/"><img src="../img/icone-site 2.png"/>
+					</a>
+					</div>
+					</div>
+					</div>
+					</div>
+					</div>
+					</div>
+					</div>
+					</div><br><br>
+
+					<div class="imagem01"> <br>
+					<div class="container-fluid">
+					<div class="row">
+					<div class="col-md-12">
+
+					<br><br><br><br><br>
+
+
+
+					<div style="text-align: center;">
+					<button type="button" class="btn btn-danger ">
+						<a  style="color: white" target="_blank" href="http://10.10.0.14:3000/d/U-4uEffnz/links-matriz?orgId=1&refresh=30s"><h2><b>DASHBOARDS</b></h2></a>
+					</button>
+					</div>
+					<br><br><br>
+
+
+					<div class="row">
+					
+						<div class="col-md-4">
+							<iframe src="http://10.10.0.14:3000/d/U-4uEffnz/links-matriz?orgId=1&refresh=30s&from=1646292969507&to=1646314569507&theme=dark&viewPanel=2" width=100% height="200" frameborder="0">  </iframe>
+						</div>
+						
+						<div class="col-md-4">
+							<iframe src="http://10.10.0.14:3000/d/U-4uEffnz/links-matriz?orgId=1&refresh=30s&from=1646293009117&to=1646314609117&theme=dark&viewPanel=3" width=100% height="200" frameborder="0">  </iframe>
+						</div>
+					
+						<div class="col-md-4">
+							<iframe src="http://10.10.0.14:3000/d/U-4uEffnz/links-matriz?orgId=1&refresh=30s&from=1646293049634&to=1646314649634&theme=dark&viewPanel=4" width=100% height="200" frameborder="0">  </iframe>
+						</div>
+
+					</div>
+
+					<br><br>
+
+					<div class="row">
+					
+						<div class="col-md-4">
+							<iframe src="http://10.10.0.14:3000/d/U-4uEffnz/links-matriz?orgId=1&refresh=30s&from=1646292969507&to=1646314569507&theme=light&viewPanel=2" width=100% height="200" frameborder="0">  </iframe>
+						</div>
+						
+						<div class="col-md-4">
+							<iframe src="http://10.10.0.14:3000/d/U-4uEffnz/links-matriz?orgId=1&refresh=30s&from=1646293009117&to=1646314609117&theme=light&viewPanel=3" width=100% height="200" frameborder="0">  </iframe>
+						</div>
+					
+						<div class="col-md-4">
+							<iframe src="http://10.10.0.14:3000/d/U-4uEffnz/links-matriz?orgId=1&refresh=30s&from=1646293049634&to=1646314649634&theme=light&viewPanel=4" width=100% height="200" frameborder="0">  </iframe>
+						</div>
+
+					</div>
+					</div>
+					</div>	
+					</div>
+					</div>
+
+					
+					
+					<div class="container-fluid" style="background-color: #77dd77">
+					<div class="row" style="color: black">
+					<div class="col-md-12">
+						
+						<h1 class="text-center"><br>
+						<b>BOA COZINHA</b>
+						</h1><br>
+						
+					</div>
+					</div>
+					</div>
+
+								
+		
+
+					<div class="body"> <br><br>
+					<div class="container-fluid">
+					<div class="row">
+					<div class="col-md-12">
+					<div class="row">
+									
+					<div class="col-md-6" style="text-align: center;"><br><br><br>
+					<div class="card-body" >
+					<p style="text-align: center;"><img src="../src/img/boacozinha.png" width=60%;></p>
+					</div>
+					</div><br><br> <br><br> <br><br>
+								
+								
+					<div class="col-md-6" style="text-align: center;"><br><br><br><br><br><br><br><br><br>
+					
+					<button class="btn btn-success" style="color: black"> 
+						<h3>CONFIRA O CARDÁPIO SEMANAL</h3>
+					</button><br><br><br><br>
+					<button class="btn btn-success">
+						<a style="color: black" href="cozinha.php"><h2><b>CLIQUE AQUI</b></h2></a>
+					</button>
+					</div>
+					</div><br><br><br><br><br><br>
+					</div>
+					</div>
+					</div>
+
+					<div class="body"> <br><br>
+					<div class="container-fluid" style="background-color: #a9a9a9">
+					<div class="row" style="color: black">
+					<div class="col-md-12">
+						<div class="container">
+							<br>
+							<h1 class="text-center"><b> NOSSOS ANÚNCIOS</b></h1>
+							<br>
+						</div>
+					</div>
+					</div>
+					</div>
+
+
+					
+					<div class="imagem03">
+					<div class="container-fluid">
+					<div class="row">
+					<div class="col-md-12"> <br><br><br>
+					<div class="row">
+											
+					<div class="col-md-6" style="text-align: center;"><br><br><br>
+					<div class="card-body" >
+					<br><br><br>
+					</div>
+
+					
+					<h1 style="color: white"><b> CLASSIFICADOS </b></h1>
+					
+										
+					<p style="color: black" ><br><br><br>
+					<p>
+
+					<!--<a style="color: white" target="_blank" href="https://form.jotform.com/220885530419660"><h2>CLICK AQUI PARA ANÚNCIAR O SEU PRODUTO</h2></a> -->
+
+					<a style="color: white" target="_blank" href="https://app.ideacrm.com.br/Home.aspx"><h2>Click AQUI e abra um chamado no <b style="color: black">IDEA</b> para anunciar o seu produto.</h2></a>
+
+					
+					</p> </p><br> <br> <br> <br>
+											
+					<a style="color: white" target="_blank" href="classificados.php"><h2>CLIQUE AQUI E CONFIRA NOSSOS ANÚNCIOS</h2> </a>
+					</div>
+											
+											
+					<div class="col-md-6"><br>
+					<div class="btn">
+					<h5 style="text-align: center;"></h5>
+					<div class="card-body" >
+					<p id="header-inner" style="text-align: center;">
+					<img src="img/anuncie_aqui_01.jpg" width=100%;>
+					</p>
+					</div>
+					</div>
+					</div>
+					</div>
+					</div>
+					</div>
+					</div>
+					</div>
+					
+	
+			
+ 
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+
+	
+</body>
+	
+
+	<footer>
+			<div  class="container-auto">
+				<div style="background-color: white" ><br>
+					<h4 style="color: black; text-align: center;"><b>&copy; CONCRESERV 2022 </h4>
+					<h5 style="color: red; text-align: center;"> Todos os direitos reservados.</h5> </p>
+					<h5 style="color: black; text-align: center;"><b>
+						As fotos aqui veiculadas, logotipo e marca são de propriedade do site. 
+						É vetada a reprodução, total ou parcial dos conteúdos. </h5> 
+					
+
+					<a href='https://contador.s12.com.br'> 
+						<button class="btn">
+							<img src="/img/iconeview.png"> <img src='https://contador.s12.com.br/img-YD6b18A8CdxbcWc1-21.gif'>
+						</button>
+					
+						<script 
+							type='text/javascript' 
+							src='https://contador.s12.com.br/ad.js?id=YD6b18A8CdxbcWc1'> 
+						</script>
+					</a>
+
+				</div> 
+			</div>                            
+	</footer>
+	
+
+
+</html>
